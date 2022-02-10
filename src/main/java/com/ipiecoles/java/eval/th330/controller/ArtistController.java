83bb7c1b56d66df41d190baedd07c36372489aeb
Julien <kalaycijulien@gmail.com>
2022-02-10 16:48:56 +0100

    barre recherche marche coté template, mais coté controller voir si il faut renvoyer page ou modelAndView

--- src/main/java/com/ipiecoles/java/eval/th330/controller/ArtistController.java
+++ src/main/java/com/ipiecoles/java/eval/th330/controller/ArtistController.java
@@ -1,12 +1,23 @@
 package com.ipiecoles.java.eval.th330.controller;
 
+import com.ipiecoles.java.eval.th330.model.Artist;
 import com.ipiecoles.java.eval.th330.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.persistence.EntityNotFoundException;
 
 @Controller
 public class ArtistController {
@@ -31,4 +42,44 @@ public class ArtistController {
         model.addObject("artist",artistService.findById(id));
         return model;
     }
+
+    /*@RequestMapping(
+            method = RequestMethod.GET,
+            value = "/employes",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            params = "matricule"
+    )
+    public ModelAndView searchByMatricule(
+            @RequestParam String name
+    ){
+        ModelAndView model = new ModelAndView("detail");
+        model.addObject("employe", artistService.findByNameLikeIgnoreCase(name));
+        return model;
+    }*/
+
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            params = "name"
+
+    )
+    public Page<Artist> findArtistByName(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(defaultValue = "name") String sortProperty,
+            @RequestParam(defaultValue = "ASC") String sortDirection,
+            @RequestParam String name
+    ){
+        Page artists =  artistService.findByNameLikeIgnoreCase(name, page,size,sortDirection,sortProperty);
+        if(artists != null){
+            return artists;
+        }
+
+        throw new EntityNotFoundException("L'artiste de nom " + name + " n'a pas été trouvé !");
+    }
+
+
+
 }
--- src/main/java/com/ipiecoles/java/eval/th330/service/ArtistService.java
+++ src/main/java/com/ipiecoles/java/eval/th330/service/ArtistService.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -57,4 +59,8 @@ public class ArtistService {
         Pageable pageable = PageRequest.of(page,size,sort);
         return artistRepository.findByNameContainingIgnoreCase(name, pageable);
     }
+
+
+//    @Query(value = "SELECT * FROM artist a WHERE a.name LIKE :name% ", nativeQuery = true)
+//    Page<Artist> findByName(@Param("name") String name, Pageable pageable);
 }

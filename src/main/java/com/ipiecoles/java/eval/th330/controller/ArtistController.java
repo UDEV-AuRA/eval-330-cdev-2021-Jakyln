1c6a562971f286a4c596e0ef3bb6e449a7ce69e3
Julien <kalaycijulien@gmail.com>
2022-02-10 16:08:30 +0100

    nom modifié dans template

--- src/main/java/com/ipiecoles/java/eval/th330/controller/ArtistController.java
+++ src/main/java/com/ipiecoles/java/eval/th330/controller/ArtistController.java
@@ -3,6 +3,7 @@ package com.ipiecoles.java.eval.th330.controller;
 import com.ipiecoles.java.eval.th330.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,5 +20,15 @@ public class ArtistController {
     public String accueil(){
 
         return "accueil";
+    }
+
+    @RequestMapping(
+            method =RequestMethod.GET,
+            value ="/artists/{id}"
+    )
+    public ModelAndView detailArtist(@PathVariable Long id){
+        ModelAndView model = new ModelAndView("detailArtist");
+        model.addObject("artist",artistService.findById(id));
+        return model;
     }
 }
